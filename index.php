e2ed344643ceceed61143f23a691b67fec345903
jayden0810 <9025917@student.tcrmbo.nl>
2025-05-26 12:50:24 +0200

    slyle change

--- index.php
+++ index.php
@@ -15,16 +15,22 @@ if (!isset($_SESSION['userId'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Escape Room</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
 
   <h1>Welkom</h1>
+  <p>Je probeerde even geleden "gratis" video games te downloaden via websites die je online hebt gevonden.</p>
+  <p>Sindst dien doet je computer raar en heb je geen toegang meer tot je bestanden.</p>
+  <p>Je hebt een melding gekregen dat je een escape room moet spelen om weer toegang te krijgen tot je bestanden.</p>
+  
+
+
+
   <!-- De button hieronder is alleen voor testdoeleinden bij het opstarten van het project.
      Deze pagina is bedoeld voor de uitleg van jullie escape room. -->
-
-  <button><a href="room_1.php">Klik hier voor een demonstratie van kamer 1</a></button>
+  <button><a href="room_1.php">start kamer 1</a></button>
 
 </body>
 
