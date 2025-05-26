a9d9e0f77588859ec7d55573fb56b027d6508631
jayden0810 <9025917@student.tcrmbo.nl>
2025-05-26 13:49:59 +0200

    win/verlies scherm

---
+++ WinVerlies/lose.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Escape Room</title>
+  <link rel="stylesheet" href="stylewl.css">
+</head>
+
+<body>
+<section id="loser" >
+  <h1>you lose!</h1>
+  <p>de tijd is om</p>
+
+  <p>Je computer doet het het helemaal niet meer</p>
+  <p>Je hebt gefaald</p>
+
+</section>
+
+  
+
+
+
+  <!-- De button hieronder is alleen voor testdoeleinden bij het opstarten van het project.
+     Deze pagina is bedoeld voor de uitleg van jullie escape room. -->
+  <button><a href="room_1.php">probeer opniew</a></button>
+
+</body>
+
+</html>
---
+++ WinVerlies/win.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Escape Room</title>
+  <link rel="stylesheet" href="stylewl.css">
+</head>
+
+<body>
+<section id="win" >
+  <h1>you won!</h1>
+  <p>Je computer doet het weer goed</p>
+
+  <p>Je hebt 1min en 29sec gedaan over kamer 1</p>
+  <p>Je hebt 3min en 33sec gedaan over kamer 2</p>
+  </section>
+
+  
+
+
+
+  <!-- De button hieronder is alleen voor testdoeleinden bij het opstarten van het project.
+     Deze pagina is bedoeld voor de uitleg van jullie escape room. -->
+  <button><a href="room_1.php">probeer opniew</a></button>
+
+</body>
+
+</html>
