7d563bc188ac72d8a52d0b335ac1266ba796de40
Erkin-Yilmaz <9024439@student.tcrmbo.nl>
2025-06-15 15:19:39 +0200

    Update escape-room (1).sql

--- escape-room (1).sql
+++ escape-room (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 26 mei 2025 om 12:53
+-- Gegenereerd op: 15 jun 2025 om 15:17
 -- Serverversie: 10.4.32-MariaDB
 -- PHP-versie: 8.2.12
 
@@ -41,11 +41,11 @@ CREATE TABLE `questions` (
 
 INSERT INTO `questions` (`id`, `question`, `answer`, `hint`, `roomId`) VALUES
 (16, 'Je ziet een melding: “Inbraak gedetecteerd in Firewall Protocol X-9. Herstel het patroon.”\n8, 16, 32, __, 128', '64', 'Denk aan vermenigvuldigen.', 1),
-(17, 'Je komt een map tegen genaamd \"UifsfJtOpEpctubdmf\". Decodeer de bestandsnaam.', 'ThereIsNoObstacle', 'Gebruik Caesar Cipher (verschuiving -1).', 1),
+(17, 'Je komt een map tegen genaamd \"SgdrqHrMnNarszbkd\". Decodeer de bestandsnaam.', 'ThereIsNoObstacle', 'Caesar Cipher is used (-1).', 1),
 (18, 'Welk bestand hoort niet tussen deze systeemonderdelen?\nsysconfig.dll, userdata.exe, malX_virus.exe, kernel32.dll, bootmgr.sys', 'malX_virus.exe', 'Zoek het geïnfecteerde bestand.', 1),
 (19, 'De AI stelt een logische vraag:\n\"Als A actief is, is B inactief.\nAls B actief is, is C altijd actief.\nC is momenteel actief.\nWelke modules kunnen actief zijn?\"', 'B en C', 'Denk logisch en sluit uit wat niet kan.', 2),
-(20, 'Het virus heeft de klok omgekeerd ingesteld: 13:74. Denk binair. Wat is het geheime wachtwoord?', '8911', 'Zet het binair om en draai het om.', 2),
-(21, 'Gebruik de eerste letters van je vorige antwoorden om de reboot-code te kraken.', 'STMBER', 'Gebruik de eerste letter van elk eerder antwoord.', 2);
+(20, 'Je opent een map vol verdachte bestanden.\r\nEén bestand valt op: protocol_final_v2_DELETE_THIS.txt.\r\nIn het bestand staat:\r\n\r\n“Het wachtwoord is het aantal letters in het alfabet minus het aantal klinkers in het Engelse alfabet.”\r\n\r\nWat is het wachtwoord?', '21', 'Zet het binair om en draai het om.', 2),
+(21, 'ER word gevraagt naar een reboot code wat zou het zijn?', '6421', 'alle cijfers die je hebt ingevult', 2);
 
 -- --------------------------------------------------------
 
@@ -67,7 +67,8 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`) VALUES
 (1, 'test', 'HALLO@hallo.com', '$2y$10$ELKOaBYZ1Z5/N9fYuU8npeBD8Xj/oct0L0aFBsfiCgxzLqU8vENj6', 'admin'),
-(2, 'test2', 'HALLO1@hallo.com', '$2y$10$gJOqyR7bOEDQnlf5OuL7Fe8NXwK8JJELsYSkKFfjB4cLYIuFR53tC', 'player');
+(2, 'test2', 'HALLO1@hallo.com', '$2y$10$gJOqyR7bOEDQnlf5OuL7Fe8NXwK8JJELsYSkKFfjB4cLYIuFR53tC', 'player'),
+(3, 'test3', 'HALLO2@hallo.com', '$2y$10$kYByMqY1ZwcOJZNe7jSJbe78mCw9q/.yq/7PVoFeKauhovObWRvEa', 'player');
 
 --
 -- Indexen voor geëxporteerde tabellen
@@ -100,7 +101,7 @@ ALTER TABLE `questions`
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
